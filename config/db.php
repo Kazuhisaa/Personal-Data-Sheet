e30726fa4d9dfcde7177b25d8b75d7a7a2b718d7
Jebreil Jhon Blancada <jebreilblancada20@gmail.com>
2025-09-29 22:42:47 +0800

    fixing database

--- config/db.php
+++ config/db.php
@@ -1,14 +1,12 @@
 <?php
-$host = "localhost";   // usually localhost
-$user = "root";        // default user ng XAMPP/MAMP
-$pass = "";            // default password (blank sa XAMPP)
-$db   = "personaldatasheet";   // pangalan ng database mo
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
